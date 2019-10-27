91e5cf527208cd263cc9e918d28fef15449ff2c2
Elkamel <riadhelkamel1@hotmail.fr>
2019-10-27 13:48:01 +0100

    first commit

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    { public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Shol { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
